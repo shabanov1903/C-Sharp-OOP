443b9a187c1223f32ba218465c638af8132f7845
Danil Shabanov <shabanov1903@gmail.com>
2021-10-13 21:59:42 +0300

    create class with fields

---
+++ Lesson2/L2-1/L2-1/AccountBank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2_1
+{
+    class AccountBank
+    {
+        private long check;
+        private decimal balance;
+        private CheckType checkType;
+
+        public long GetCheck() => check;
+        public void SetCheck(long value) => check = value;
+        public decimal GetBalance() => balance;
+        public void SetBalance(decimal value) => balance = value;
+        public CheckType GetCheckType() => checkType;
+        public void SetCheckType(CheckType value) => checkType = value;
+    }
+}
---
+++ Lesson2/L2-1/L2-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Внедрние зависимости для возможности быстрого переноса функционала
+            IPrint printer = new ConsolePrinter();
+
+            // Инициализация счета в банке
+            AccountBank account = new();
+            account.SetCheck(4589_3212_4565_8954);
+            account.SetBalance(50000);
+            account.SetCheckType(CheckType.Insured);
+
+            // Печать данных
+            printer.Print("Данные пользователя:");
+            printer.Print(account.GetCheck());
+            printer.Print(account.GetBalance());
+            printer.Print(account.GetCheckType());
+        }
+    }
+}
