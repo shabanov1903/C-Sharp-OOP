c6cacffb531d9f0896477b3bcd8106997cc39921
Danil Shabanov <shabanov1903@gmail.com>
2021-11-10 23:55:06 +0300

    lesson5

---
+++ Lesson5/L5-1/L5-1.Tests/L5_1_Tests_Logic.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Xunit;
+using L5_1;
+
+namespace L5_1.Tests
+{
+    public class L5_1_Tests_Logic
+    {
+        [Fact]
+        public void Test_Equals()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 2);
+            RationalNumb numb3 = new(1, 3);
+
+            Assert.True(numb1 == numb2);
+            Assert.False(numb1 == numb3);
+        }
+
+        [Fact]
+        public void Test_Not_Equals()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 3);
+            RationalNumb numb3 = new(1, 2);
+
+            Assert.True(numb1 != numb2);
+            Assert.False(numb1 != numb3);
+        }
+
+        [Fact]
+        public void Test_More()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 2);
+            RationalNumb numb3 = new(-1, 2);
+            RationalNumb numb5 = new(1, 3);
+            RationalNumb numb7 = new(-1, 3);
+            RationalNumb numb8 = new(-1, 3);
+
+            Assert.True(numb1 > numb5);
+            Assert.True(numb7 > numb3);
+            Assert.True(numb1 > numb7);
+
+            Assert.False(numb1 < numb5);
+            Assert.False(numb7 < numb3);
+            Assert.False(numb1 < numb7);
+
+            Assert.False(numb1 > numb2);
+            Assert.False(numb7 > numb8);
+        }
+
+        [Fact]
+        public void Test_Smaller()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 2);
+            RationalNumb numb3 = new(-1, 2);
+            RationalNumb numb5 = new(1, 3);
+            RationalNumb numb7 = new(-1, 3);
+            RationalNumb numb8 = new(-1, 3);
+
+            Assert.True(numb5 < numb1);
+            Assert.True(numb3 < numb7);
+            Assert.True(numb7 < numb1);
+
+            Assert.False(numb5 > numb1);
+            Assert.False(numb3 > numb7);
+            Assert.False(numb7 > numb1);
+
+            Assert.False(numb1 < numb2);
+            Assert.False(numb7 < numb8);
+        }
+
+        [Fact]
+        public void Test_More_Or_Equal()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 2);
+            RationalNumb numb3 = new(-1, 2);
+            RationalNumb numb5 = new(1, 3);
+            RationalNumb numb7 = new(-1, 3);
+            RationalNumb numb8 = new(-1, 3);
+
+            Assert.True(numb1 >= numb5);
+            Assert.True(numb7 >= numb3);
+            Assert.True(numb1 >= numb7);
+
+            Assert.False(numb1 <= numb5);
+            Assert.False(numb7 <= numb3);
+            Assert.False(numb1 <= numb7);
+
+            Assert.True(numb1 >= numb2);
+            Assert.True(numb7 >= numb8);
+        }
+
+        [Fact]
+        public void Test_Smaller_Or_Equal()
+        {
+            RationalNumb numb1 = new(1, 2);
+            RationalNumb numb2 = new(1, 2);
+            RationalNumb numb3 = new(-1, 2);
+            RationalNumb numb5 = new(1, 3);
+            RationalNumb numb7 = new(-1, 3);
+            RationalNumb numb8 = new(-1, 3);
+
+            Assert.True(numb5 <= numb1);
+            Assert.True(numb3 <= numb7);
+            Assert.True(numb7 <= numb1);
+
+            Assert.False(numb5 >= numb1);
+            Assert.False(numb3 >= numb7);
+            Assert.False(numb7 >= numb1);
+
+            Assert.True(numb1 <= numb2);
+            Assert.True(numb7 <= numb8);
+        }
+    }
+}
---
+++ Lesson5/L5-2/L5-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace L5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Исходные значения:");
+            var numb1 = new ComplexNumb(1, 2);
+            var numb2 = new ComplexNumb(1, 3);
+            Console.WriteLine(numb1.ToString());
+            Console.WriteLine(numb2.ToString());
+
+            Console.WriteLine("Сумма значений:");
+            var numbResult = numb1 + numb2;
+            Console.WriteLine(numbResult.ToString());
+
+            Console.WriteLine("Разность значений:");
+            numbResult = numb1 - numb2;
+            Console.WriteLine(numbResult.ToString());
+
+            Console.WriteLine("Произведение значений:");
+            numbResult = numb1 * numb2;
+            Console.WriteLine(numbResult.ToString());
+
+            Console.WriteLine("Произведение первого числа на 2:");
+            numbResult = numb1 * new ComplexNumb(2, 0);
+            Console.WriteLine(numbResult.ToString());
+        }
+    }
+}
